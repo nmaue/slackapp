2a815a41e2c0b83104a6d8be827f403d0072a267
Nathan Maue <nmaue@nyu.edu>
2017-07-15 22:54:19 -0400

    validating input

--- lib/slackapp.py
+++ lib/slackapp.py
@@ -1,12 +1,22 @@
+import ConfigParser
+import re
 from flask import Flask, request, jsonify, abort
+
 application = Flask(__name__)
 
 @application.route("/rt", methods=['POST'])
 def rt():
-    abort(403)
+    validate_token(request)
     text = request.form.get('text', None)
+    if not re.match("^[0-9]{6}$", text):
+    	abort(500)
     return jsonify({
 	"response_type": "in_channel",
         "text": "https://rt.tss.its.nyu.edu/Ticket/Display.html?id=" + text
     })
 
+def validate_token(request):
+	config = ConfigParser.ConfigParser()
+	config.read("../etc/token.ini")
+	if request.form.get('token', None) != config.get("Slack", "token"):
+		abort(403)
